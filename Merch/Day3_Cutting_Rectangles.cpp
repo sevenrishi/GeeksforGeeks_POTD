9513a59f050c3d0bb64fedb9cfd748778bc808b5
Saptarshi Mondal <saptarshimondal768@gmail.com>
2023-03-04 21:56:33 +0530

    Create Day3_Cutting_Rectangles.cpp

---
+++ Merch/Day3_Cutting_Rectangles.cpp
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    vector<long long int> minimumSquares(long long int L, long long int B)
+    {
+        // code here
+        long long int x=__gcd(L,B);
+        vector<long long int>ans;
+        ans.push_back((L*B)/(x*x));
+        ans.push_back(x);
+        return ans;
+
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long int L, B;
+        cin>>L>>B;
+        
+        Solution ob;
+        vector<long long int> ans = ob.minimumSquares(L, B);
+        cout<<ans[0]<<" "<<ans[1]<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
